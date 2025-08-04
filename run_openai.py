cd37c30ece66e20bf8e61b84673e3a1936b7cb9f
luigi.aceto <luigi.aceto@genre.com>
2025-08-04 16:09:54 +0200

    add openai model name

--- run_openai.py
+++ run_openai.py
@@ -1,5 +1,5 @@
 import os  
-from langchain.llms import OpenAI  
+from langchain_openai import OpenAI 
 from langchain.prompts import PromptTemplate  
   
 # Set your OpenAI API key from the environment variable  
